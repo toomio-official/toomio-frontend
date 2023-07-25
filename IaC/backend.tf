45b5ca15e2fb7014db40d97fe6a67b97cccfbd6c
Krishan Shamod <krishanshamod@gmail.com>
2023-07-26 04:13:29 +0530

    Updated Terraform configs

--- IaC/backend.tf
+++ IaC/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-toomio"
+    bucket         = "terraform-state-toomio-project"
     key            = "frontend/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock-toomio"
+    dynamodb_table = "terraform-state-lock-toomio-project"
     encrypt        = true
   }
 }
--- IaC/main.tf
+++ IaC/main.tf
@@ -1,8 +1,8 @@
 module "frontend" {
   source = "github.com/toomio-official/terraform-s3-cloudfront-module?ref=v0.1.0"
-  s3_bucket_name = "toomio-frontend-bucket"
-  cloudfront_distribution_description = "Toomio Frontend"
+  s3_bucket_name = "toomio-project-frontend-bucket"
+  cloudfront_distribution_description = "Toomio Project Frontend"
   domain_name = "toomio.com"
-  acm_certificate_arn = "arn:aws:acm:us-east-1:850515863053:certificate/496021ba-bb10-4ee3-b53c-0b0b4a98e27c"
+  acm_certificate_arn = "arn:aws:acm:us-east-1:499249761013:certificate/00ee6af5-9c96-4687-bd75-9ca3ed5de2eb"
   index_document = "index.html"
 }
