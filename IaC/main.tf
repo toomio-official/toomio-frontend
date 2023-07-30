160dc71fea53ef2badb8de5d508af049b1c462c7
Krishan Shamod <krishanshamod@gmail.com>
2023-07-30 23:37:02 +0530

    Updated Terraform module version

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,9 +1,8 @@
 module "frontend" {
-  source = "github.com/toomio-official/terraform-s3-cloudfront-module?ref=v0.2.0"
-  s3_bucket_name = "toomio-project-frontend-bucket"
+  source                              = "github.com/toomio-official/terraform-s3-cloudfront-module?ref=v0.3.3"
+  s3_bucket_name                      = "toomio-project-frontend-bucket"
   cloudfront_distribution_description = "Toomio Project Frontend"
-  domain_names = ["toomio.com", "www.toomio.com"]
-  acm_certificate_arn = "arn:aws:acm:us-east-1:499249761013:certificate/00ee6af5-9c96-4687-bd75-9ca3ed5de2eb"
-  index_document = "index.html"
-  error_document = "error.html"
+  domain_names                        = ["toomio.com", "www.toomio.com"]
+  acm_certificate_arn                 = "arn:aws:acm:us-east-1:499249761013:certificate/00ee6af5-9c96-4687-bd75-9ca3ed5de2eb"
+  index_document                      = "index.html"
 }
