2a99dbe78ff0b9b59854f071d92bd6cc13ebc985
savitarasitha <asithanuwanbandara@gmail.com>
2023-09-20 10:45:10 +0530

    Implement otherprofile component

--- src/app/view/main-ui/home/otherprofile/otherprofile.component.ts
+++ src/app/view/main-ui/home/otherprofile/otherprofile.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {VisibilityService} from "../visibility.service";
 
 @Component({
   selector: 'app-otherprofile',
@@ -8,7 +7,33 @@ import {VisibilityService} from "../visibility.service";
 })
 export class OtherprofileComponent {
 
-  constructor(private visibilityService:VisibilityService) {
+  journey:boolean = false;
+  post:boolean = true;
+
+  constructor() {
+  }
+
+
+  postMethod(post1: HTMLDivElement, journey1: HTMLDivElement) {
+    this.journey = false;
+    this.post = true;
+    post1.classList.add('active');
+    journey1.classList.remove('active');
+  }
+
+  journeyMethod(post1: HTMLDivElement, journey1: HTMLDivElement) {
+    this.journey = true;
+    this.post = false;
+    journey1.classList.add('active');
+    post1.classList.remove('active');
+  }
+
+  journeyMenu(dropj: HTMLDivElement) {
+    dropj.classList.toggle('visible');
+  }
+
+  visiblePost(visible_post: HTMLDivElement) {
+    visible_post.classList.toggle('postInJourney');
   }
 
 }
