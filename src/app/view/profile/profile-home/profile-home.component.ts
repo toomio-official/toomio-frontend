b73a36a49c4b97f1defdae5b195481510f3f06b7
asithanuwanbandara@gmail.com <asithanuwanbandara@gmail.com>
2024-08-11 21:39:09 +0530

    create profile home

--- src/app/view/profile/profile-home/profile-home.component.ts
+++ src/app/view/profile/profile-home/profile-home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {PostService} from "../../../services/post.service";
 import {Location} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-profile-home',
@@ -9,14 +10,25 @@ import {Location} from "@angular/common";
 })
 export class ProfileHomeComponent implements OnInit{
 
-  selectedOption: string = '';
+  // selectedOption: string = '';
   userEmail: string | null = null;
   journeyList:any
-  postList:any
+  postList:any;
+  selectedOption: string = 'post';
+  // postList: any[] = [];
+  selectedPostId: string = '';
+  isCommentModalOpen: boolean = false;
+  comments: any[] = [];
+  content: any;
+  likePostData: any;
+  likeCount:any;
+
 
   constructor(
     private postService: PostService,
-    private location: Location
+    private location: Location,
+    private router: Router,
+
   ) {
   }
 
@@ -27,8 +39,18 @@ export class ProfileHomeComponent implements OnInit{
     this.getAllJourneys();
   }
 
-  selectOption(option: string): void {
+  // selectOption(option: string): void {
+  //   this.selectedOption = option;
+  // }
+
+  selectOption(option: string) {
     this.selectedOption = option;
+    if (option === 'post') {
+      this.getAllPosts();
+    } else if (option === 'journey') {
+      this.getAllJourneys();
+      // You can call the journey-related method here if needed
+    }
   }
 
   goBack(): void {
@@ -37,23 +59,74 @@ export class ProfileHomeComponent implements OnInit{
 
   getAllPosts(){
     this.postService.getAllPosts(this.userEmail).subscribe((res:any)=>{
-      this.postList= res
+      this.postList= res;
+      this.postList.forEach((post: any) => {
+        post.likeCount = post.likes.length; // Set likeCount for each post
+        post.commentCount = post.comments.length
+      });
     })
   }
 
   getAllJourneys(){
     this.postService.getAllJourneys(this.userEmail).subscribe((res:any)=>{
       this.journeyList = res;
-      // for (var i = 0; i < res.length; i++) {
-      //   this.journeyList.push({
-      //     label:
-      //     res[i].title,
-      //
-      //     value:  res[i].title,
-      //   });
-      // }
+      this.journeyList.forEach((post: any) => {
+        post.likeCount = post.likes.length; // Set likeCount for each post
+        post.commentCount = post.comments.length
+      });
     })
   }
 
+  likePost(data: any) {
+    let obj = {
+      smPostId: data._id,
+      userEmail: this.userEmail
+    }
+
+    this.postService.likePost(obj).subscribe((res: any) => {
+      this.likePostData = res;
+      this.getPostLikes(obj)
+    })
+  }
+
+  addComment(data:any) {
+    let obj= {
+      smPostId: data._id,
+      userEmail: data.userEmail,
+      content: this.content
+    }
+    this.postService.commentPost(obj).subscribe((res:any)=>{
+      this.comments = res;
+      this.content = '';
+      this.closeCommentModal()
+    })
+  }
+
+  openCommentModal(data:any) {
+    this.isCommentModalOpen = true;
+    this.postService.getAllPostComments(data._id).subscribe((res:any)=>{
+      this.comments = res;
+      debugger
+    })
+
+  }
+
+  closeCommentModal() {
+    this.isCommentModalOpen = false;
+    this.comments = []
+  }
+
+
+  getPostLikes(data: any) {
+    this.postService.getPostLikes(data._id).subscribe((res: any) => {
+      this.likeCount = res.count;
+      debugger
+    })
+  }
+
+  findProfile(){
+    debugger
+    this.router.navigate(['/admin/profile/find-user-profile'])
+  }
 
 }
